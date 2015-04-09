942b6bf833a037fe06e3f9cc5684d00da2ed2621
Sorawa <thepowerover@163.com>
2015-04-09 16:35:15 +0800

    AddTargetRequestWithHeaderFile

--- core/common/page/page.go
+++ core/common/page/page.go
@@ -132,6 +132,13 @@ func (this *Page) AddTargetRequests(urls []string, respType string) *Page {
     return this
 }
 
+
+// AddTargetRequest adds one new Request with header file for waitting for crawl.
+func (this *Page) AddTargetRequestWithHeaderFile(url string, respType string, headerFile string) *Page {
+	this.targetRequests = append(this.targetRequests, request.NewRequestWithHeaderFile(url, respType, headerFile))
+	return this
+}
+
 // AddTargetRequest adds one new Request waitting for crawl.
 // The respType is "html" or "json" or "jsonp" or "text".
 // The urltag is name for marking url and distinguish different urls in PageProcesser and Pipeline.
